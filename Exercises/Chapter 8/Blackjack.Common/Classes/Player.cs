db2feea3ceeabe037dca2df85461bf10ded31190
Jonas Fagerberg <ceshewnut@gmail.com>
2022-05-23 13:10:51 +0200

    Blackjack (Class library): Refactored to class library and added a test project and test methods.

---
+++ Exercises/Chapter 8/Blackjack.Common/Classes/Player.cs
@@ -0,0 +1,52 @@
+using BlackJack.Records;
+using BlackJack.Enums;
+namespace BlackJack.Classes;
+
+class Player
+{
+    Blackjack Game { get; set; }
+    public bool Stays { get; set; }
+    public Card[] Cards { get; private set; } = new Card[0];
+    public int Score { get; set; }
+    public PlayerTypes PlayerType { get; init; }
+    public Results Result { get; private set; } = Results.Unknown;
+
+    public Player(PlayerTypes playerType, Blackjack game) => (PlayerType, Game) = (playerType, game);
+
+    void CalculateScore()
+    {
+        Score = Cards.Where(c => !c.IsHidden).Sum(c => c.Value);
+        var aces = Cards.Where(c => c.Value.Equals(1) && !c.IsHidden); // Fetch all aces
+
+        // The score is low enough to add the ace with its high value of 11.
+        // Because 1 has already been added with the Sum function, only 10 is added.
+        foreach(var ace in aces)
+            if (Score <= 11) Score += 10;
+    }
+
+    public void AddCard(Card[] cards)
+    {
+        Cards = Cards.Concat(cards).ToArray();
+
+        if(PlayerType.Equals(PlayerTypes.Player))
+        {
+            CalculateScore();
+            if(Score == 21 && cards.Count().Equals(2))
+            {
+                Result = Results.BlackJack;
+                Game.Stay();
+            }
+            if (Score > 21)
+            {
+                Result = Results.PlayerLost;
+                Game.Stay();
+            }
+        }
+        else
+        {
+            if (!Stays) cards[0].IsHidden = true;
+            CalculateScore();
+            if (Score > 21) Result = Results.DealerLost;
+        }
+    }
+}
---
+++ Exercises/Chapter 8/Blackjack.Common/Records/Card.cs
@@ -0,0 +1,17 @@
+namespace BlackJack.Records;
+
+public record Card
+{
+    const string _back = "🂠";
+    string _symbol = string.Empty;
+    public bool IsHidden { get; set; }
+    public string Symbol { get { return IsHidden ? _back : _symbol; } }
+    public int Value { get; set; }
+    public bool IsRemoved { get; set; } = false;
+    public Card(string symbol, int value, bool isHidden = false)
+    {
+        _symbol = symbol;
+        Value = value;
+        IsHidden = isHidden;
+    }
+}
